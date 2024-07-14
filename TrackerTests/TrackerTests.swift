72bf6818c4369dbcfc8ad83b1092e31f487f6f0f
Karina <barakhaeva@inbox.ru>
2024-07-14 19:35:26 +0300

    Test completed

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import SnapshotTesting
+@testable import Tracker
+
+final class TrackerTests: XCTestCase {
+    func testViewController() {
+        let vc = TrackersViewController()
+        
+        assertSnapshot(of: vc, as: .image)                                            
+    }
+}
